3eb3bc09230ce90351b688e5d4bbff480faac380
Roderick de Nijs <rsdenijs@vers-dc-analytics-1.sachit.intern>
2015-05-11 09:19:58 +0200

    set width of widget

--- pandas_visualize/pandas_visualize.py
+++ pandas_visualize/pandas_visualize.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 from IPython.utils.traitlets import link
 
-def iview(self):
-    f = lambda rows_start,rows_end,cols: display(self[list(cols)].iloc[rows_start:rows_end])
-    rs = IntSlider(min=0,max=self.shape[0])
-    re = IntSlider(min=0,max=self.shape[0])
+def iview(self, width = 400):
+    f = lambda rows_start, rows_end,cols: display(self[list(cols)].iloc[rows_start:rows_end])
+    rs = IntSlider(min=0, max=self.shape[0], width=width)
+    re = IntSlider(min=0, max=self.shape[0], width= width)
     link((re, 'min'), (rs, 'value'))
     interact(f,
              cols=SelectMultiple(options=self.columns.tolist()),
              rows_start=rs,
-             rows_end=re)
+             rows_end=re, width=width)
 
 pd.DataFrame.iview = iview
